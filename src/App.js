346c088c4d227820f54d98492d1f8775652da1a0
varad4545 <godvarad@gmail.com>
2023-09-08 17:41:39 +0530

    added:login:ui-fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AddProductPage from './components/product';
 import OrderPage from './components/order';
+import LoginPage from './components/login';
 import { Routes, Route } from 'react-router-dom';
 
 
@@ -9,6 +10,7 @@ const App = () => {
       <div>
         <Routes>
            <Route path="/" element={<OrderPage />} />
+           <Route path="/login" element={<LoginPage />} />
            <Route path="/add-product" element={<AddProductPage />} />
         </Routes>
       </div>
